d8d965977bc36322649ecea16c299038298259f5
micjef <michaeljeffrysetiawan@gmail.com>
2022-04-04 14:17:13 +0700

    show html using nodejs

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require("http"),
+  fs = require("fs");
+
+fs.readFile(__dirname + "/../public/index.example.html", function (err, html) {
+  if (err) {
+    throw err;
+  }
+  http
+    .createServer(function (request, response) {
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      response.write(html);
+      response.end();
+    })
+    .listen(8000);
+});
+
+//show server
+console.log("Server running at http://locahost:8000/");
